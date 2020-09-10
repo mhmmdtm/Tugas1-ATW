107d300473f2af1e85b26c272a53e150e3c5e09e
mhmmdtm <71063647+mhmmdtm@users.noreply.github.com>
2020-09-10 22:23:49 +0700

    Menambah class anjing

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
- <?php
+<?php
 
- echo "Animal <br>";
+echo "Animal <br>";
+echo "<hr>";
 
 class Kucing{
  	public $nama, $jumlah_kaki, $bisa_terbang, $suara;
@@ -10,7 +11,7 @@ class Kucing{
  $Kucing = new Kucing;
  $Kucing->nama = "Mueza";
  $Kucing->jumlah_kaki = "4";
- $Kucing->bisa_terbang = "tidak bisa terbang";
+ $Kucing->bisa_terbang = "tidak";
  $Kucing->suara = "meoong";
 
  echo "Kucing <br>";
@@ -19,3 +20,22 @@ class Kucing{
  echo "Bisa Terbang : $Kucing->bisa_terbang <br>";
  echo "Suara : $Kucing->suara <br>";
  echo "<hr>";
+
+class Anjing{
+ 	public $nama, $jumlah_kaki, $bisa_terbang, $suara;
+
+ }
+
+ $Anjing = new Anjing;
+ $Anjing->nama = "Diggie";
+ $Anjing->jumlah_kaki = "4";
+ $Anjing->bisa_terbang = "tidak";
+ $Anjing->suara = "guk guk";
+
+ echo "Anjing <br>";
+ echo "Nama : $Anjing->nama <br>";
+ echo "Jumlah Kaki : $Anjing->jumlah_kaki <br>";
+ echo "Bisa Terbang : $Anjing->bisa_terbang <br>";
+ echo "Suara : $Anjing->suara <br>";
+ echo "<hr>";
+
